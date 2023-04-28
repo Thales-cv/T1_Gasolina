4cd35718e2671f506c05a2417bb5d922eb73df66
Thales Veiga <thales.cveiga@gmail.com>
2023-04-28 10:23:25 -0300

    init test

--- src/test/testes.java
+++ src/test/testes.java
@@ -1,7 +1,26 @@
 public class testes{
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+    
+
+    System.out.println("teste!");
+    // Testes de mistura (TESTE PARAMETRO)
+    //1. Retirada com combustível suficiente
+    //2. Retirada com combustível insuficiente
+    //3. Retirada com combustível suficiente e valores finais do tanque atualizdos
+
+
+    // Testes de Estado (TESTE VALOR LIMITE)
+    //1. Combustível acima de 50% -> estado NORMAL
+    //2. Combustível menor que 50% e maior que 25% -> estado SOBREAVISO
+    //3. Combusítvel menor de 25% -> estado EMERGÊNCIA
+
+    // Testes de Pedidos (TESTE BASEADOS EM MODELOS)
+    //1. Quando tipo do posto for COMUM & ESTADOS
+    //2. Quando tipo de posto for ESTRATÉGICO & ESTADOS
+
+    
+
     }
 
 }
